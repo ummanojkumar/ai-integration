4da877797c67ca8da1719979c31a7ca7eae6c6a7
offline computer <ummanojkumar925@gmail.com>
2024-05-31 19:18:19 +0530

    adding project Structure

---
+++ src/main/java/com/manoj/ai/ai_integration/AiIntegrationApplication.java
@@ -0,0 +1,15 @@
+package com.manoj.ai.ai_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiIntegrationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AiIntegrationApplication.class, args);
+		System.out.println("manoj started");
+	}
+
+}
---
+++ src/test/java/com/manoj/ai/ai_integration/AiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.ai.ai_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
